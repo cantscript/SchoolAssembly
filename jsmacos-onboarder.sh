996296b3bcf5797916ac84284e81373c9a2441f5
darlow86 <anthony.darlow@me.com>
2024-03-29 17:06:50 +0000

    First test script to use plist. Added new plist for testing with main script

--- jsmacos-onboarder.sh
+++ jsmacos-onboarder.sh
@@ -96,6 +96,26 @@ cmdLog=/var/tmp/dialog.log
 installoPath=/usr/local/Installomator/Installomator.sh
 installoOptions="NOTIFY=silent BLOCKING_PROCESS_ACTION=ignore INSTALL=force IGNORE_APP_STORE_APPS=yes LOGGING=REQ"
 
+### Preferences Reader Func ###
+
+MANAGED_PREFERENCE_DOMAIN="com.jsmacos.onboarder2"
+
+getPref() { # $1: key, $2: default value, $3: domain
+	local key=${1:?"key required"}
+	local defaultValue=${2-:""}
+	local domain=${3:-"$MANAGED_PREFERENCE_DOMAIN"}
+	
+	value=$(osascript -l JavaScript \
+		-e "$.NSUserDefaults.alloc.initWithSuiteName('$domain').objectForKey('$key').js")
+	
+	if [[ -n $value ]]; then
+		echo $value
+	else
+		echo $defaultValue
+	fi
+}
+
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 #                              Automator Labels                                  #
 #                             Format needs to be:-                               #
@@ -106,13 +126,79 @@ installoOptions="NOTIFY=silent BLOCKING_PROCESS_ACTION=ignore INSTALL=force IGNO
 # Example: googlechromepkg,Google\ Chrome,/Users/ladmin/Desktop/Chrome\ Icon.png #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-apps=(googlechromepkg,Google\ Chrome,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Chrome\ Icon.png
-	microsoftexcel,Excel,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Excel\ Icon.png
-	microsoftword,Word,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Word\ Icon.png
-	microsoftoutlook,Outlook,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Outlook\ Icon.png
-	microsoftteams,Teams,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Teams\ Icon.png
-	microsoftonedrive,One\ Drive,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/OneDrive\ Icon.png
-)
+#apps=(googlechromepkg,Google\ Chrome,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Chrome\ Icon.png
+#	microsoftexcel,Excel,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Excel\ Icon.png
+#	microsoftword,Word,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Word\ Icon.png
+#	microsoftoutlook,Outlook,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Outlook\ Icon.png
+#	microsoftteams,Teams,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Teams\ Icon.png
+#	microsoftonedrive,One\ Drive,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/OneDrive\ Icon.png
+#)
+
+######################################
+#  Get details of Apps to Install    #
+# from plist / profile and turn into #
+#             an array               #
+######################################
+
+### Logging Option ###
+### 0=Standard 1= Verbose ###
+installAppLogging=0
+
+### Dump plist key into a variable ###
+appList=$(getPref "AppInstalls")
+
+### Change IFS for workflow ###
+oldIFS=
+IFS=,
+
+### Logic to define how many apps are to be installed ###
+echo "-> Starting..."
+echo "Converting Prefs into script Vars"
+installArray=($appList)
+installItemsNo=${#installArray[@]}
+let requiredApps=$installItemsNo/3
+echo "There are $installItemsNo items, resulting in $requiredApps Apps to be installed"
+echo "-----------"
+
+### Pre-flight varibles ###
+apps=()
+appCounter=1
+indexPoint=0
+
+### Create App Install array for Switdialog ###
+while [ $appCounter -le $requiredApps ]; do
+	apps+=(${installArray[@]:$indexPoint:3})
+	echo "R:$requiredApps | I: $indexPoint | Added App: $appCounter "
+	indexPoint=$(($indexPoint+3))
+	((appCounter++))
+done
+echo "The resulting array contains ${#apps[@]} Apps to be installed"
+
+
+### Success / Error Message ###
+echo "-----------"
+if [ $requiredApps = ${#apps[@]} ]; then
+	echo "SUCCESS: $requiredApps apps where read from Prefs & ${#apps[@]} Apps have been added to Onboarder script"
+else
+	echo "ERROR: $requiredApps apps where read from Prefs but ${#apps[@]} Apps have been added to Onboarder script"
+fi
+
+if [ $installAppLogging != 0 ]; then
+	for items in "${apps[@]}"; do
+		echo "----------"
+		echo "Icon Location: "$( echo "$items" | cut -d ':' -f2 | cut -d '"' -f1 | tr -d '\')""
+		echo "Display Name: "$(echo "$items" | cut -d ':' -f3 | cut -d '"' -f1)""
+		echo "Installomator Label: "$(echo "$items" | cut -d ':' -f4 | cut -d '"' -f1 | tr -d '\')""
+		
+	done
+fi
+
+### Return IFS to original state ###
+IFS=$oldIFS
+
+### End Process ###
+echo "----------"
+echo "<- Ending..."
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 #                              Monitor App Store Installs                            #
@@ -124,11 +210,11 @@ apps=(googlechromepkg,Google\ Chrome,/Library/Application\ Support/macOS\ App\ O
 # Example: Keynote,/Applications/Keynote.app,/Users/ladmin/Desktop/Keynote\ Icon.png #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-storeInstalls=(Keynote,/Applications/Keynote.app,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Keynote\ Icon.png
-	Numbers,/Applications/Numbers.app,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Numbers\ Icon.png
-	Pages,/Applications/Pages.app,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Pages\ Icon.png
-	Swift\ Playgrounds,/Applications/Playgrounds.app,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Playgrounds\ Icon.png
-	Jamf\ Student,/Applications/Jamf\ Student.app,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Student\ Icon.png
+storeInstalls=(Keynote,/Applications/Keynote.app,/Applications/Keynote.app/Contents/Resources/AppIcon.icns
+	#Numbers,/Applications/Numbers.app,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Numbers\ Icon.png
+	#Pages,/Applications/Pages.app,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Pages\ Icon.png
+	#Swift\ Playgrounds,/Applications/Playgrounds.app,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Playgrounds\ Icon.png
+	#Jamf\ Student,/Applications/Jamf\ Student.app,/Library/Application\ Support/macOS\ App\ Onboarder/App\ Icons/Student\ Icon.png
 )
 
 ####Personalise Window Text with logged-in user or computer name####
@@ -213,7 +299,7 @@ fi
 ####Add titles to be installed with Installomator to list and set status to waiting####
 sleep 2
 for title in "${apps[@]}"; do
-	echo "listitem: add, title: "$(echo "$title" | cut -d ',' -f2)", icon: "$(echo "$title" | cut -d ',' -f3)", statustext: waiting, status: wait " >> $cmdLog
+	echo "listitem: add, title: "$(echo "$title" | cut -d ':' -f3 | cut -d '"' -f1)", icon: "$(echo "$title" | cut -d ':' -f2 | cut -d '"' -f1)", statustext: waiting, status: wait " >> $cmdLog
 done 
 echo "listitem: delete, title: Onboarding Starting...." >> $cmdLog
 
@@ -226,24 +312,24 @@ echo "progress: $progressCount" >> $cmdLog
 ####Install Apps using Intstallomator####
 for app in "${apps[@]}"; do
 	sleep 0.5
-	echo "listitem: title: "$(echo "$app" | cut -d ',' -f2)", status: pending, statustext: Installing" >> $cmdLog
-	echo "progresstext: Install of "$(echo "$app" | cut -d ',' -f2)" in Progress" >> $cmdLog
+	echo "listitem: title: "$(echo "$app" | cut -d ':' -f3 | cut -d '"' -f1)", status: pending, statustext: Installing" >> $cmdLog
+	echo "progresstext: Install of "$(echo "$app" | cut -d ':' -f3 | cut -d '"' -f1)" in Progress" >> $cmdLog
 	if [ $runOption = 0 ]; then
-		runInstallo="$(sudo $installoPath "$(echo "$app" | cut -d ',' -f1)" $installoOptions)"
+		runInstallo="$(sudo $installoPath "$(echo "$app" | cut -d ':' -f4 | cut -d '"' -f1)" $installoOptions)"
 	else
-		runInstallo="$($installoPath "$(echo "$app" | cut -d ',' -f1)" $installoOptions)"
+		runInstallo="$($installoPath "$(echo "$app" | cut -d ':' -f4 | cut -d '"' -f1)" $installoOptions)"
 	fi 
 	exitStatus="$( echo "${runInstallo}" | grep --binary-files=text -i "exit" | tail -1 | sed -E 's/.*exit code ([0-9]).*/\1/g' || true )"
 	if [[ ${exitStatus} -eq 0 ]] ; then
-		echo "listitem: title: "$(echo "$app" | cut -d ',' -f2)", status: success, statustext: Installed" >> $cmdLog
+		echo "listitem: title: "$(echo "$app" | cut -d ':' -f3 | cut -d '"' -f1)", status: success, statustext: Installed" >> $cmdLog
 		progressCount=$(( progressCount + 1)) 
 		echo "progress: $progressCount" >> $cmdLog
-		echo "progresstext: Install of "$(echo "$app" | cut -d ',' -f2)" complete" >> $cmdLog
+		echo "progresstext: Install of "$(echo "$app" | cut -d ':' -f3 | cut -d '"' -f1)" complete" >> $cmdLog
 	else
-		echo "listitem: title: "$(echo "$app" | cut -d ',' -f2)", status: error, statustext: Installation Error" >> $cmdLog
+		echo "listitem: title: "$(echo "$app" | cut -d ':' -f3 | cut -d '"' -f1)", status: error, statustext: Installation Error" >> $cmdLog
 		progressCount=$(( progressCount + 1)) 
 		echo "progress: $progressCount" >> $cmdLog
-		echo "progresstext: Install of "$(echo "$app" | cut -d ',' -f2)" Error installation" >> $cmdLog
+		echo "progresstext: Install of "$(echo "$app" | cut -d ':' -f3 | cut -d '"' -f1)" Error installation" >> $cmdLog
 	fi	
 	#Rest before next item
 	sleep 1
